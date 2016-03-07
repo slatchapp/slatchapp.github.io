478af3c613718c2014d79909576e2bbe8b182b61
slatchapp <lukejanoslatchapp@gmail.com>
2016-03-06 19:34:50 -0500

    Update application.js

--- application.js
+++ application.js
@@ -1,4 +1,10 @@
-var c = document.getElementById("appcanvas");
-var ctx = c.getContext("2d");
-ctx.fillStyle = "#FF00FF";
-ctx.fillRect(0,0,100,100);
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+$(document).start(function(){
+  var c = document.getElementById("appcanvas");
+  var ctx = c.getContext("2d");
+  ctx.fillStyle = "#FF00FF";
+  ctx.fillRect(0,0,100,100);
+)};
