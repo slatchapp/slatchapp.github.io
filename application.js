11be02f460f1932a1365adb92d233e4e1bcbe638
slatchapp <lukejanoslatchapp@gmail.com>
2016-03-07 16:34:44 -0500

    Update application.js

--- application.js
+++ application.js
@@ -1,6 +1,6 @@
 var mainbounds = function() {
   ctx.fillStyle = "#EDDBFF";
-  //ctx.fillRect(0,0,600,500);
+  ctx.fillRect(0,0,600,500);
   ctx.fillStyle = '#FFFFFF';
   ctx.fillRect(25, 50, 550, 425);
   ctx.font = "30px Sans Serif";
