7a3044c89b5ace2e90ba99c0833672256e1c359b
slatchapp <lukejanoslatchapp@gmail.com>
2016-03-06 20:24:15 -0500

    Update application.js

--- application.js
+++ application.js
@@ -1,8 +1,14 @@
-var initialrun = function() {
-  var c = document.getElementById("appcanvas");
-  var ctx = c.getContext("2d");
+var mainbounds = function() {
   ctx.fillStyle = "#EDDBFF";
-  ctx.fillRect(0,0,600,100);
-  ctx.font = "30px Sans Serif"
-  ctx.strokeText("Slatch",10,30)
+  ctx.fillRect(0,0,600,500);
+  ctx.filStyle = '#FFFFFF';
+  ctx.fillRect(25, 50, 575, 475);
+  ctx.font = "30px Sans Serif";
+  ctx.strokeText("Slatch",10,30);
+}
+
+var initialrun = function() {
+  c = document.getElementById("appcanvas");
+  ctx = c.getContext("2d");
+  mainbounds();
 }
