994483bba3e5dc5c10a308d3f739ab118752dce9
slatchapp <lukejanoslatchapp@gmail.com>
2016-03-06 19:37:59 -0500

    Update application.js

--- application.js
+++ application.js
@@ -1,10 +1,1 @@
-var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-$(document).start(function(){
-  var c = document.getElementById("appcanvas");
-  var ctx = c.getContext("2d");
-  ctx.fillStyle = "#FF00FF";
-  ctx.fillRect(0,0,100,100);
-)};
+//This will be the application file
