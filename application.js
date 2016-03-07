ac47a8b7aaa5aa774d3e0e4c8d945dfd3009dc93
slatchapp <lukejanoslatchapp@gmail.com>
2016-03-07 16:34:34 -0500

    Update application.js

--- application.js
+++ application.js
@@ -1,8 +1,8 @@
 var mainbounds = function() {
   ctx.fillStyle = "#EDDBFF";
   //ctx.fillRect(0,0,600,500);
-  ctx.filStyle = '#FFFFFF';
-  ctx.fillRect(25, 50, 575, 475);
+  ctx.fillStyle = '#FFFFFF';
+  ctx.fillRect(25, 50, 550, 425);
   ctx.font = "30px Sans Serif";
   ctx.strokeText("Slatch",10,30);
 }
